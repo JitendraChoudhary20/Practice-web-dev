8e398258cd2915ea004bb748e35fae2f307f705a
Jitendra Choudhary <jituchoudhary20@gmail.com>
2022-12-07 13:57:07 +0530

    some more event

--- event.js
+++ event.js
@@ -5,4 +5,12 @@ function displayDate(){
 document.getElementById("para").innerHTML= Date();
 };
 
-
+document.getElementById("btn2").onmouseover=logger;
+function logger(){
+    document.getElementById("demo").innerHTML="this is logged";
+}
+document.getElementById("btn2").onmousedown=logger2;
+function logger2(){
+    var a = document.getElementById("demo");
+    console.log(a);
+}
